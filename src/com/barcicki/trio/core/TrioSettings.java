13cbc837768c934af9e552b7bb146f652411bdfd
Artur Barcicki <barcicki@gmail.com>
2012-04-06 10:30:58 +0200

    trio settings

---
+++ src/com/barcicki/trio/core/TrioSettings.java
@@ -0,0 +1,8 @@
+package com.barcicki.trio.core;
+
+public class TrioSettings {
+	public static String APP_ID = "";
+	public static String APP_NAME = "Trio";
+	public static String APP_KEY = "";
+	public static String APP_SECRET = "";
+}
