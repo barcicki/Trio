abb44c6f729eb897c535caa5d516fd9cc326bcbd
Artur Barcicki <barcicki@gmail.com>
2012-04-12 18:35:15 +0200

    fixes and openfeint settings

--- src/com/barcicki/trio/animations/ReverseInterpolator.java
+++
@@ -1,9 +0,0 @@
-package com.barcicki.trio.animations;
-
-import android.view.animation.Interpolator;
-
-public class ReverseInterpolator implements Interpolator {
-    public float getInterpolation(float paramFloat) {
-        return Math.abs(paramFloat -1f);
-    }
-}
